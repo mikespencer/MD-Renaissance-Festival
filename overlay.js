c8baf4a2593c78d016fe7e6688d5351c61ceeb6e
Mike Spencer <michael.spencer@wpost.com>
2011-08-24 16:44:40 -0400

    adjusted positioning to work propery if window is smaller than document and user is scrolled left

--- overlay.js
+++ overlay.js
@@ -3,11 +3,10 @@
 }
 
 var wpAd = wpAd || {};
-		wpAd.overlay = wpAd.overlay || {};
-		wpAd.overlay.bg_opacity = 0.6;
-		wpAd.overlay.fade_speed = 400;
-		wpAd.overlay.isOpen = false;
-		wpAd.overlay.firstPlay = true;
+wpAd.overlay = wpAd.overlay || {};
+wpAd.overlay.bg_opacity = 0.6;
+wpAd.overlay.fade_speed = 400;
+wpAd.overlay.isOpen = false;
 
 wpAd.overlay.thisMovie = function(movieName){
 	if (navigator.appName.indexOf("Microsoft") != -1) {
@@ -23,6 +22,7 @@ wpAd.overlay.beginAnimation = function(){
 }
 
 wpAd.overlay.replay = function(){
+	wpAd.overlay.remove(); // make it impossible for there to be 2 overlays present at once
 	wpAd.overlay.thisMovie('MDRF2011_HPoverlay').resetAnimation();
 	wpAd.overlay.thisMovie('MDRF2011_LBoverlay').resetAnimation();
 	wpAd.overlay.exec();
@@ -36,7 +36,6 @@ wpAd.overlay.remove = function(){
 			$(this).parent().remove();
 		});
 		$('#ad_overlay,  #ad_overlay_close').hide();
-		wpAd.overlay.firstPlay = false;	
 		wpAd.overlay.beginAnimation();
 	}
 }
@@ -49,18 +48,17 @@ wpAd.overlay.exec = function(){
 			$close = $('#ad_overlay_close'),
 			w = $window.width(),
 			h = $window.height(),
+			leftPos = $window.scrollLeft(),
 			swf = 'http://media.washingtonpost.com/wp-adv/advertisers/mdrenfest/2011/overlay.swf?clickTag='+wpAd.overlay.ct,
 			creative_code = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="'+w+'" height="'+h+'" name="overlay_swf" id="overlay_swf" align="middle">\
 			<param name="movie" value="'+swf+'" />\
 			<param name="quality" value="high" />\
 			<param name="bgcolor" value="#ffffff" />\
 			<param name="play" value="true" />\
-			<param name="loop" value="true" />\
 			<param name="wmode" value="transparent" />\
 			<param name="scale" value="noscale" />\
 			<param name="menu" value="true" />\
 			<param name="devicefont" value="false" />\
-			<param name="salign" value="" />\
 			<param name="allowScriptAccess" value="always" />\
 			<!-\-[if !IE]>-\->\
 			<object type="application/x-shockwave-flash" data="'+swf+'" width="'+w+'" height="'+h+'">\
@@ -68,12 +66,10 @@ wpAd.overlay.exec = function(){
 				<param name="quality" value="high" />\
 				<param name="bgcolor" value="#ffffff" />\
 				<param name="play" value="true" />\
-				<param name="loop" value="true" />\
 				<param name="wmode" value="transparent" />\
 				<param name="scale" value="noscale" />\
 				<param name="menu" value="true" />\
 				<param name="devicefont" value="false" />\
-				<param name="salign" value="" />\
 				<param name="allowScriptAccess" value="always" />\
 			<!-\-<![endif]-\->\
 				<!--a href="http://www.adobe.com/go/getflash">\
@@ -83,14 +79,12 @@ wpAd.overlay.exec = function(){
 			</object>\
 			<!-\-<![endif]-\->\
 		</object>';
-
-	if(!wpAd.overlay.firstPlay ){
-		$('#ad_overlay_close, #ad_overlay_bg, #ad_overlay').css({top:$window.scrollTop()+'px'})
-	}
-
+	
+	$('#ad_overlay_close, #ad_overlay_bg, #ad_overlay').css({top:$window.scrollTop()+'px', left : leftPos + 'px'});
+	
 	$('#ad_overlay_bg').css({opacity:wpAd.overlay.bg_opacity, height: $window.height() + 'px'}).fadeIn(wpAd.overlay.fade_speed, function(){
 		$('#ad_overlay').html(creative_code);
-		$close.css({left : (w - $close.outerWidth())+'px', display : 'block'}).click(wpAd.overlay.remove);
+		$close.css({left : ((w + leftPos) - $close.outerWidth())+'px', display : 'block'}).click(wpAd.overlay.remove);
 		wpAd.overlay.timer = setTimeout(wpAd.overlay.remove, 7000);
 	});
 }
@@ -100,13 +94,13 @@ wpAd.overlay.init = function(){
 		$(function(){
 			/*if(typeof getCookie === 'function' && !getCookie('mdrenfest')){
 				try{
-					var expire = new Date();
-					expire.setDate(expire.getDate()+1);
-					setCookie('mdrenfest','true',expire.toUTCString())
+					var exp = new Date();
+					exp.setDate(e.getDate()+1);
+					setCookie('mdrenfest','true',exp.toUTCString())
 				} catch(e){}
 				wpAd.overlay.exec();
 			} else{
-				wpAd.overlay.firstPlay = false;	
+				//wpAd.overlay.firstPlay = false;	
 				wpAd.overlay.beginAnimation();
 			}*/
 			wpAd.overlay.exec();
