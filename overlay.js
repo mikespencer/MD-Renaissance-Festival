5be3542a4f9694e7fb4398cd90e80d698d8628ff
Mike Spencer <michael.spencer@wpost.com>
2011-08-27 18:01:31 -0400

    flash detect and backup images added. cookie will always expire at 12:00am midnight that night.

--- overlay.js
+++ overlay.js
@@ -93,19 +93,33 @@ wpAd.overlay.exec = function(){
 		wpAd.overlay.timer = setTimeout(wpAd.overlay.remove, 7000);
 	});
 }
-
+wpAd.overlay.flashDetect = function() {
+	var i,a,o,p,s="Shockwave",f="Flash",t=" 2.0",u=s+" "+f,v=s+f+".",rSW=RegExp("^"+u+" (\\d+)");
+	if((o=navigator.plugins)&&(p=o[u]||o[u+t])&&(a=p.description.match(rSW)))return a[1];
+	else if(!!(window.ActiveXObject))for(i=10;i>0;i--)try{if(!!(new ActiveXObject(v+v+i)))return i}catch(e){}
+	return 0;
+}
 wpAd.overlay.init = function(){
 	if(typeof jQuery !== 'undefined'){
 		$(function(){
-			if(typeof getCookie === 'function' && typeof commercialNode !== 'undefined' && getCookie('mdrenfest_'+commercialNode) !== commercialNode){
+			if(wpAd.overlay.flashDetect() >= 8){
+				if(typeof getCookie === 'function' && typeof commercialNode !== 'undefined' && getCookie('mdrenfest_'+commercialNode) !== commercialNode){
+					try{
+						var exp = new Date();
+						exp.setDate(exp.getDate()+1);
+						exp.setHours(0,0,0,0);
+						setCookie('mdrenfest_'+commercialNode,commercialNode,exp.toUTCString())
+					} catch(e){}
+					wpAd.overlay.exec();
+				} else{
+					wpAd.overlay.beginAnimation();
+				}
+			}
+			else{
 				try{
-					var exp = new Date();
-					exp.setDate(exp.getDate()+1);
-					setCookie('mdrenfest_'+commercialNode,commercialNode,exp.toUTCString())
+					$('#slug_leaderboard div:first').html('<a href="'+wpAd.overlay.ct+'" target=_blank"><img src="http://media.washingtonpost.com/wp-adv/advertisers/mdrenfest/2011/MDRF2011_LBoverlay.png" width="728" height="90" alt="" style="border:0;" />');
+					$('#slug_flex_ss_bb_hp div:first').html('<a href="'+wpAd.overlay.ct+'" target=_blank"><img src="http://media.washingtonpost.com/wp-adv/advertisers/mdrenfest/2011/MDRF2011_HPoverlay.jpg" width="336" height="850" alt="" style="border:0;" /></a>');
 				} catch(e){}
-				wpAd.overlay.exec();
-			} else{
-				wpAd.overlay.beginAnimation();
 			}
 		})
 	}	else{
